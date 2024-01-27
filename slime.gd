8022fdb13163c06556f05ed9c1b30b918b9ab5d2
AmogusFan113 <IDraw4rt@gmail.com>
2024-01-27 21:23:20 +0000

    Slime now moves towards the player :)

--- slime.gd
+++ slime.gd
@@ -8,8 +8,9 @@ func _ready():
 	target = get_tree().get_nodes_in_group("player")[0]
 	$Sprite2D.play("running")
 
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity = (target.position - position).normalized() * 50
+	move_and_slide()
 
 func damage(d):
 	hp -= d
