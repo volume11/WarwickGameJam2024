dc83e27ffe1ce91ed1b6d0b4a703fe66d3e23882
Amy Rinaldi <idraw4rt@gmail.com>
2024-01-27 18:22:35 +0000

    Add player attacking structure

--- player.gd
+++ player.gd
@@ -2,6 +2,14 @@ extends CharacterBody2D
 
 @export var speed = 300
 
+var target
+
+func _process(delta):
+	target = get_tree().get_nodes_in_group("enemy")[0]
+	if (target != null && Input.is_action_pressed("attack") && $AttackTimer.is_stopped()):
+		print("attack")
+		$AttackTimer.start()
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = direction * speed
