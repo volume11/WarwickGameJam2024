92586f099d95c440d5c5c939b52f068425aebd65
AmogusFan113 <IDraw4rt@gmail.com>
2024-01-27 22:26:40 +0000

    add base stats to player

--- player.gd
+++ player.gd
@@ -2,17 +2,30 @@ extends CharacterBody2D
 
 signal health_changed(health: int)
 
-@export var speed = 300
-
 var target
 
-var health = 10
-var max_health = 10
+var base_hp = 10
+var base_attack_speed = 0.5
+var base_strength = 1
+var base_speed = 300
+
+var health
+var max_health
+var attack_speed
+var strength
+var speed
 
 var currentWeapon
 
 func _ready():
+	#temporary
 	currentWeapon = $Weapons/BroadSword
+	
+	health = base_hp
+	max_health = health
+	strength = base_strength
+	speed = base_speed
+	attack_speed = base_attack_speed
 
 func _process(delta):
 	var nodes = get_tree().get_nodes_in_group("enemy")
@@ -22,7 +35,7 @@ func _process(delta):
 		target = null
 		
 	if (target != null && Input.is_action_pressed("attack") && $AttackTimer.is_stopped()):
-		$AttackTimer.start()
+		$AttackTimer.start(base_attack_speed)
 		currentWeapon.attack(target)
 
 func _physics_process(delta):
