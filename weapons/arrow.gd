d24da8fb01ae7218381c26935b358353789c2be5
AmogusFan113 <IDraw4rt@gmail.com>
2024-01-27 22:58:23 +0000

    minor logic mistake

--- weapons/arrow.gd
+++ weapons/arrow.gd
@@ -6,9 +6,8 @@ func _ready():
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if (collision):
-		if (collision.get_collider()):
-			var a = collision.get_collider()
-			if (a.is_in_group("enemy")):
-				a.damage(1)
-			
-			queue_free()
+		var a = collision.get_collider()
+		if (a.is_in_group("enemy")):
+			a.damage(1)
+		
+		queue_free()
