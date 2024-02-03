f7a2ceb41e57b8a720011946e76ef26dfe4af207
Amy Rinaldi <idraw4rt@gmail.com>
2024-02-03 14:48:47 +0000

    Add new fields to the weapon resource (texture for the inventory and a unique id for the weapon)

--- weapons/weapon.gd
+++ weapons/weapon.gd
@@ -1,6 +1,8 @@
 class_name Weapon extends Resource
 
+@export var id: String
 @export var weapon_name: String
 @export var damage: int
 @export var attack_speed: float
 @export var durability: int = 10
+@export var texture: Texture2D
